0c02f509f9fa0ec5b3e7cbee2821bd373212000e
Aniket Agrawal <ankt7987@gmail.com>
2024-07-06 22:05:16 +0530

    final

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,72 +1,266 @@
 
 const showMenu = (toggleId, navId) =>{
-   const toggle = document.getElementById(toggleId),
-         nav = document.getElementById(navId)
-
-   toggle.addEventListener('click', () =>{
-      
-       nav.classList.toggle('show-menu')
-       toggle.classList.toggle('show-icon')
-   })
-}
-
-showMenu('nav-toggle','nav-menu')
-
-
-const dropdownItems = document.querySelectorAll('.dropdown__item')
-
-
-dropdownItems.forEach((item) =>{
-    const dropdownButton = item.querySelector('.dropdown__button') 
-
-    
-    dropdownButton.addEventListener('click', () =>{
-   
-        const showDropdown = document.querySelector('.show-dropdown')
-        
-        
-        toggleItem(item)
-
-      
-        if(showDropdown && showDropdown!== item){
-            toggleItem(showDropdown)
-        }
-    })
-})
-
-
-const toggleItem = (item) =>{
-  
-    const dropdownContainer = item.querySelector('.dropdown__container')
-
-   
-    if(item.classList.contains('show-dropdown')){
-        dropdownContainer.removeAttribute('style')
-        item.classList.remove('show-dropdown')
-    } else{
-        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
-        item.classList.add('show-dropdown')
-    }
-}
-
-
-const mediaQuery = matchMedia('(min-width: 1118px)'),
-      dropdownContainer = document.querySelectorAll('.dropdown__container')
-
-
-const removeStyle = () =>{
-   
-    if(mediaQuery.matches){
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
        
-        dropdownContainer.forEach((e) =>{
-            e.removeAttribute('style')
-        })
-
+        nav.classList.toggle('show-menu')
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+ 
+ 
+ const dropdownItems = document.querySelectorAll('.dropdown__item')
+ 
+ 
+ dropdownItems.forEach((item) =>{
+     const dropdownButton = item.querySelector('.dropdown__button') 
+ 
+     
+     dropdownButton.addEventListener('click', () =>{
+    
+         const showDropdown = document.querySelector('.show-dropdown')
+         
+         
+         toggleItem(item)
+ 
+       
+         if(showDropdown && showDropdown!== item){
+             toggleItem(showDropdown)
+         }
+     })
+ })
+ 
+ 
+ const toggleItem = (item) =>{
+   
+     const dropdownContainer = item.querySelector('.dropdown__container')
+ 
+    
+     if(item.classList.contains('show-dropdown')){
+         dropdownContainer.removeAttribute('style')
+         item.classList.remove('show-dropdown')
+     } else{
+         dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+         item.classList.add('show-dropdown')
+     }
+ }
+ 
+ 
+ const mediaQuery = matchMedia('(min-width: 1118px)'),
+       dropdownContainer = document.querySelectorAll('.dropdown__container')
+ 
+ 
+ const removeStyle = () =>{
+    
+     if(mediaQuery.matches){
         
-        dropdownItems.forEach((e) =>{
-            e.classList.remove('show-dropdown')
-        })
-    }
-}
-
-addEventListener('resize', removeStyle)
+         dropdownContainer.forEach((e) =>{
+             e.removeAttribute('style')
+         })
+ 
+         
+         dropdownItems.forEach((e) =>{
+             e.classList.remove('show-dropdown')
+         })
+     }
+ }
+ 
+ addEventListener('resize', removeStyle)
+ //---------------------------------------------------------------------hide nav
+ const nav = document.querySelector(".header");
+ nav.classList.add("nav--hidden");
+ let lastScrollY = window.scrollY;
+ let isHovering = false;
+ let scrollTimeout;
+ 
+ function handleScroll() {
+     const currentScrollY = window.scrollY;
+ 
+     
+     if (!isHovering) {
+         if (lastScrollY < currentScrollY && currentScrollY) {
+             nav.classList.add("nav--hidden");
+         } else {
+             nav.classList.remove("nav--hidden");
+         }
+     }
+ 
+     clearTimeout(scrollTimeout);
+     scrollTimeout = setTimeout(() => {
+         if (!isHovering) {
+             nav.classList.add("nav--hidden");
+         }
+     }, 1500);                                                              //change it for increasing delay before navbar goes off
+ 
+     lastScrollY = currentScrollY;
+ }
+ 
+ 
+ window.addEventListener("scroll", handleScroll);
+ 
+ 
+ nav.addEventListener("mouseenter", () => {
+     isHovering = true;
+     nav.classList.remove("nav--hidden");
+ });
+ 
+ nav.addEventListener("mouseleave", () => {
+     isHovering = false;
+     handleScroll(); 
+ });
+ 
+ 
+ //// smoooth scrolling
+ class App {
+     constructor() {
+ 
+         this.heroImages = [...document.querySelectorAll('.hero__images img')];
+         this.texts = [...document.querySelectorAll('.text__effect')];
+         this._initialize();
+         this._render();
+     }
+ 
+     _initialize() {
+         this._setInitialStates();
+         this._createLenis();
+         this._createIntro();
+         this._createHero();
+         this._createTextAnimation();
+         this._createPinnedSection();
+     }
+ 
+ 
+     // .text__effect p,
+     _setInitialStates() {
+         gsap.set('.hero__title span, .fullwidth-image__text', {
+ 
+             y: 400,                                                 //changeable
+             opacity: 0
+         })
+         gsap.set('.hero__images img', {
+             opacity: 0,
+             y: gsap.utils.random(2000, 1900)                     //changeable
+ 
+         })
+         gsap.set('.fullwidth-image img', {
+ 
+             scale: 1.3
+         })
+ 
+ 
+     }
+ 
+     _createLenis() {
+         this.lenis = new Lenis({
+             lerp: 0.1
+         })
+     }
+ 
+     _createIntro() {
+         const tl = gsap.timeline();
+ 
+         tl.to('.hero__title div', {
+             opacity: 1
+         }).to('.hero__title span', {
+             y: 400,                                           //changeable
+             opacity: 1,
+             ease: 'expo.out',
+             duration: 2,
+             stagger: 0.01
+         }).to('.hero__images img',
+             {
+                 opacity: 1,
+                 y: gsap.utils.random(1200, 1100),
+                 ease: 'power3.out',
+                 duration: 2,
+                 stagger: 0.04
+             }, '-=1')                                          //changeable(0.5) maybe not
+     }
+ 
+     _createHero() {
+         const tl = gsap.timeline({
+             scrollTrigger: {
+                 trigger: '.hero',
+                 start: 'top top',
+                 end: 'bottom top',
+                 scrub: true,                           //changeable
+             }
+         });
+ 
+         this.heroImages.forEach(image => {
+             tl.to(image, {
+                 ease: 'none',
+                 yPercent: gsap.utils.random(-100, -50)
+             }, 0)
+         })
+ 
+     }
+ 
+     _createTextAnimation() {
+         const tl = gsap.timeline({
+             scrollTrigger: {
+                 trigger: '.text-block',
+                 start: 'top center',
+                 end: 'bottom top+=25%',
+                 scrub: true,                           //changeable
+             }
+         });
+ 
+         this.texts.forEach((text, index) => {
+             const overlay = text.querySelector('.text__overlay');
+ 
+             tl.to(overlay, {
+                 scaleX: 0
+             })
+             // .to(content,{
+             //     y:0,
+             //     opacity: 1,
+             //     ease: 'expo.out',
+             //     duration: 2,
+             //     delay:() => index*0.1
+             // },0)
+         })
+     }
+     _createPinnedSection() {
+         const tl = gsap.timeline({
+             scrollTrigger: {
+                 trigger: '.fullwidth-image',
+                 start: 'top top',
+                 end: '+=1500',
+                 scrub: true,
+                 pin: true                     //changeable
+             }
+         });
+ 
+         tl.to('.fullwidth-image__overlay', {
+ 
+             opacity: 0.7
+         }).to('.fullwidth-image', {
+ 
+ 
+             "clip-path": "polygon(0% 0% ,100% 0%,100% 100%, 0% 100%)"
+         }, 0).to('.fullwidth-image img', {
+             scale: 1
+         }, 0).to('.fullwidth-image__text',
+             {
+                 y: 0,
+                 opacity: 1
+             }, 0)
+     }
+     _render(time) {
+         this.lenis.raf(time);
+         requestAnimationFrame(this._render.bind(this))
+     }
+ 
+ }
+ 
+ new App();
+ 
+ function closeModal(){
+     let modal=document.querySelector('.modalnav');
+     modal.classList.add('modalHide');
+ }
--- script.js
+++ script.js
@@ -1,93 +1,235 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main-div'),
-//     smooth: true
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    // Define elements
+    let nalgene = document.getElementById('nalgene');
+    let hill1 = document.getElementById('hill1');
+    let hill2 = document.getElementById('hill2');
+    let person = document.getElementById('person');
+    let bottle = document.getElementById('bottle');
 
-let nalgene = document.getElementById('nalgene');
-let hill1 = document.getElementById('hill1');
-let hill2 = document.getElementById('hill2');
-let person = document.getElementById('person');
-let bottle = document.getElementById('bottle');
+    // Scroll event for parallax effect
+    window.addEventListener('scroll', () => {
+        let value = window.scrollY;
 
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-
-    nalgene.style.marginTop = value * -2 + 'px';
-    hill1.style.top = value * 0.3 + 'px';
-    hill2.style.top = value * 0.4 + 'px';
-    person.style.top = value * 0.3 + 'px';
-})
-
-gsap.to("#bottle1",{
-    scale:18,
-    x:300,
-    y:-40,
-    duration:5,
-    rotate:45,
-    scrollTrigger:{
-        trigger:"#bottle1",
-        scroller:"body",
-        start:"top 80%",
-        end:"top 80%",
-        scrub:3,            
-        pin:true            
-    }
-})
-
-gsap.from("#about-image",{
-    x:350,
-    opacity:0,
-    duration:5,
-    scrollTrigger:{
-        trigger:"#about-image",
-        scroller:"body",
-        start:"top 40%",
-        end:"top 40%",
-        scrub:3,            
-        pin:true            
-    }
-})
-
-gsap.from("#why-image",{
-    y:100,
-    opacity:0,
-    duration:5,
-    scrollTrigger:{
-        trigger:"#why-image",
-        scroller:"body",
-        start:"top 100%",
-        end:"top 100%",
-        scrub:3,            
-        pin:true            
-    }
-})
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.to(".why", {
-    yPercent: -100,
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".aboutus",
-        start: "top top",
-        end: "bottom top",
-        scrub: true
-    }
-});
-
-document.querySelectorAll('.cat').forEach(cat => {
-    const video = cat.querySelector('.hover-video');
-    const img = cat.querySelector('img');
-
-    cat.addEventListener('mouseenter', () => {
-        img.style.opacity = '0';
-        video.play();
+        nalgene.style.marginTop = value * -2 + 'px';
+        hill1.style.top = value * 0.3 + 'px';
+        hill2.style.top = value * 0.4 + 'px';
+        person.style.top = value * 0.3 + 'px';
     });
 
-    cat.addEventListener('mouseleave', () => {
-        img.style.opacity = '1';
-        video.pause();
-        video.currentTime = 0;
+    // GSAP Animations
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to("#bottle1", {
+        scale: 18,
+        x: 300,
+        y: -40,
+        duration: 5,
+        rotate: 45,
+        scrollTrigger: {
+            trigger: "#bottle1",
+            scroller: "body",
+            start: "top 80%",
+            end: "top 80%",
+            scrub: 3,
+            pin: true
+        }
+    });
+
+    gsap.from(".about", {
+        x: 350,
+        opacity: 0,
+        duration: 5,
+        scrollTrigger: {
+            trigger: ".about",
+            scroller: "body",
+            start: "top 40%",
+            end: "top 40%",
+            scrub: 3,
+            pin: true
+        }
+    });
+
+    gsap.from("#why-image", {
+        y: -100,
+        opacity: 0,
+        duration: 5,
+        scrollTrigger: {
+            trigger: "#why-image",
+            scroller: "body",
+            start: "top 80%",
+            end: "top 90%",
+            scrub: 3,
+            pin: true
+        }
+    });
+
+    gsap.to(".why", {
+        yPercent: -100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".aboutus",
+            start: "top top",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    // GSAP Scroll Animations
+    gsap.from(".ift1", {
+        scale: 0,
+        delay: 1,
+        duration: 2,
+        scrollTrigger: {
+            trigger: ".ift1",
+            scroller: "body",
+            start: "top 10%",
+            scrub: 2,
+            pin: true
+        }
+    });
+
+    gsap.from(".ift2", {
+        scale: 0,
+        delay: 1,
+        duration: 2,
+        scrollTrigger: {
+            trigger: ".ift2",
+            scroller: "body",
+            start: "top 20%",
+            scrub: 2
+        }
+    });
+
+    gsap.from(".ift3", {
+        scale: 0,
+        delay: 1,
+        duration: 2,
+        scrollTrigger: {
+            trigger: ".ift3",
+            scroller: "body",
+            start: "top 20%",
+            scrub: 2
+        }
+    });
+
+    gsap.from(".adventures", {
+        scale: 0,
+        delay: 1,
+        duration: 2,
+        scrollTrigger: {
+            trigger: ".adventures",
+            scroller: "body",
+            start: "top 100%",
+            end: "top 60%",
+            scrub: 2
+        }
+    });
+
+    gsap.from(".newsletter-container", {
+        y: 100,
+        opacity: 0,
+        duration: 3,
+        scrollTrigger: {
+            trigger: ".newsletter-container",
+            scroller: "body",
+            start: "top 70%",
+            end:"top 70%",
+            scrub: 3
+        }
+    });
+
+    gsap.from(".footer", {
+        y: 100,
+        opacity: 0,
+        duration: 3,
+        scrollTrigger: {
+            trigger: ".footer",
+            scroller: "body",
+            start: "top 70%",
+            end:"top 70%",
+            scrub: 2
+        }
+    });
+
+    // Slider functionality
+    let items = document.querySelectorAll('.slider123 .list123 .item123');
+    let next = document.getElementById('next123');
+    let prev = document.getElementById('prev123');
+    let thumbnails = document.querySelectorAll('.thumbnail123 .item123');
+
+    let countItem = items.length;
+    let itemActive = 0;
+
+    next.onclick = function() {
+        itemActive = (itemActive + 1) % countItem;
+        showSlider();
+    };
+
+    prev.onclick = function() {
+        itemActive = (itemActive - 1 + countItem) % countItem;
+        showSlider();
+    };
+
+    let refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000);
+
+    function showSlider() {
+        let itemActiveOld = document.querySelector('.slider123 .list123 .item123.active123');
+        let thumbnailActiveOld = document.querySelector('.thumbnail123 .item123.active123');
+        if (itemActiveOld) itemActiveOld.classList.remove('active123');
+        if (thumbnailActiveOld) thumbnailActiveOld.classList.remove('active123');
+
+        items[itemActive].classList.add('active123');
+        thumbnails[itemActive].classList.add('active123');
+
+        clearInterval(refreshInterval);
+        refreshInterval = setInterval(() => {
+            next.click();
+        }, 5000);
+    }
+
+    thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', () => {
+            itemActive = index;
+            showSlider();
+        });
+    });
+
+
+    const likeBtns = document.querySelectorAll(".like-btn");
+
+    likeBtns.forEach((likeBtn) =>{
+        likeBtn.addEventListener("click",() => {
+            if(likeBtn.classList.contains("active")){
+                likeBtn.classList.remove("active");
+            }else{
+                likeBtn.classList.add("active");
+            }
+        });
+    });
+
+    // Swiper initialization
+    var swiper = new Swiper(".swiper", {
+        grabCursor: true,
+        freeMode: false,
+        speed: 400,
+        mousewheel: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            draggable: true,
+        },
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            520: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            960: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            }
+        }
     });
 });
-
